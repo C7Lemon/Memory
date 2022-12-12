d9261f45b38ce5f24043e6a6a30b65aa0d6fcf41
C7Lemon <cedric.paetzold@itc-studenten.de>
2022-12-12 14:57:20 +0100

    JS für Punktestand ändern und Spielernamen ändern und paar andere Sachen

--- main.js
+++ main.js
@@ -3,3 +3,32 @@ function deckeAuf() {
     img.src = "images/test2";
     return false;
 }
+
+function namenSetzen1() {
+    let name = prompt("Spieler1 bitte Namen eingeben: ", "Dr. Ian Malcolm");
+    if (name != null) {
+        document.getElementById("name1").innerHTML =
+            name;
+    }
+}
+function namenSetzen2() {
+    let name = prompt("Spieler2 bitte Namen eingeben: ", "Dr. Alan Grant");
+    if (name != null) {
+        document.getElementById("name2").innerHTML =
+            name;
+    }
+}
+
+
+function punkteSetzen1(punkte1) {
+    if (punkte1 != null) {
+        document.getElementById("punkte1").innerHTML =
+            punkte1;
+    }
+}
+function punkteSetzen2(punkte2) {
+    if (punkte2 != null) {
+        document.getElementById("punkte2").innerHTML =
+            punkte2;
+    }
+}
